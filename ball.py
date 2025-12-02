07fc12c224006a8ae3f3d1a0bb46b92f484e5255
FakeZero(Laptop) <sjlimdaa@naver.com>
2025-12-02 17:50:01 +0900

    Ball 클래스 추가

---
+++ ball.py
@@ -0,0 +1,15 @@
+from pico2d import *
+
+class Ball:
+    def __init__(self, x, y):
+        self.image = None
+        self.x = x
+        self.y = y
+        if self.image is None:
+            self.image = load_image('ball21x21.png')
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self):
+        pass
