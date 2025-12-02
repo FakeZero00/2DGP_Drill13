58642e5c0751b1d72883459d61e1399bee5bb784
FakeZero(Laptop) <sjlimdaa@naver.com>
2025-12-02 17:53:56 +0900

    update, draw 함수 추가

--- ball.py
+++ ball.py
@@ -8,6 +8,12 @@ class Ball:
         if self.image is None:
             self.image = load_image('ball21x21.png')
 
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
